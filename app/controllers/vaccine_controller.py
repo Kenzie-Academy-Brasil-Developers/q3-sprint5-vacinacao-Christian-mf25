c71945503fd5cc2cc617ae2efd2342a3a72907ec
Christian <christian.mf22@gmail.com>
2022-02-10 15:40:07 -0300

    feat: adicionada função para ignorar chaves extras enviadas

--- app/controllers/vaccine_controller.py
+++ app/controllers/vaccine_controller.py
@@ -1,15 +1,28 @@
 from app.models.vaccine_model import Vaccine
 from sqlalchemy.exc import IntegrityError
-from datetime import datetime, timedelta
 from app.configs.database import db
 from flask import request, jsonify
 
 request_keys = ["cpf", "name", "vaccine_name", "health_unit_name"]
 
 def create_vaccine_card():
-	date = datetime.now()
 	data = request.get_json()
 	data_keys = data.keys()
+	right_keys = []
+
+	for key in request_keys:
+		if key in data_keys:
+			right_keys.append(key)
+		else:
+			return {"error": f"missing key '{key}'"}
+	
+
+	if len(right_keys) == 4:
+		delete_keys = list(data_keys - request_keys)
+		
+		for key in delete_keys:
+			del data[key]
+		
 
 	for values in data.values():
 		if type(values) is not  str:
@@ -17,8 +30,6 @@ def create_vaccine_card():
 		
 	try:
 		if len(data["cpf"]) == 11:
-			data["first_shot_date"] = date
-			data["second_shot_date"] = date + timedelta(days=90)
 			data["name"] = data["name"].title()
 			data["health_unit_name"] = data["health_unit_name"].title()
 			vaccine_card = Vaccine(**data)
